89347d62cde82fe3f631c07c236a85eeeeb53969
DanielHernandez37 <710113@student.dusd.net>
2023-02-09 11:38:58 -0800

    pt 1

---
+++ PlayerControlHernandezP4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 20;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
