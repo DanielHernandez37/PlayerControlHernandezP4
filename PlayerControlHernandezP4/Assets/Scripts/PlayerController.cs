7f13819b39b7def12b19e91b45b2556116439958
DanielHernandez37 <710113@student.dusd.net>
2023-02-27 11:42:08 -0800

    started challenge

--- PlayerControlHernandezP4/Assets/Scripts/PlayerController.cs
+++ PlayerControlHernandezP4/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 30.0f;
+    private float speed = 25.0f;
     private float turnSpeed = 100.0f;
     private float horizontalInput;
     private float forwardInput;
