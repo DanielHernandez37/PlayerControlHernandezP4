3443c6db1a07423f25efb85d96d8c9df6c9ca030
DanielHernandez37 <710113@student.dusd.net>
2023-03-01 20:42:27 -0800

    finishe challenege

--- PlayerControlHernandezP4/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ PlayerControlHernandezP4/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
+    private Vector3 offset = new Vector3(30, 0, 10);
 
     // Start is called before the first frame update
     void Start()
@@ -13,8 +13,8 @@ public class FollowPlayerX : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
+    // Update is called once per frame 
+    void LateUpdate()
     {
         transform.position = plane.transform.position + offset;
     }
